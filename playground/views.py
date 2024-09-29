ebae17177489d63b84159647565b8909a56f7604
keshab.khanal <keshab.khanal@stud.tu-darmstadt.de>
2024-09-30 00:59:28 +0200

    color

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Ashim'})
